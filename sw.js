b60400a6562f156d8b608ee05a31bf84e728837d
Fatih <nzvdgzygrk@privaterelay.appleid.com>
2026-07-25 06:38:14 +0200

    App-Version für Routenkorrektur erhöhen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "2026-07-25-2";
+const APP_VERSION = "2026-07-25-3";
 const CACHE_NAME = `turkiye-yolculuk-${APP_VERSION}`;
 const FILES_TO_CACHE = [
   `./?appv=${APP_VERSION}`,
